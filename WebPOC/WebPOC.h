93f17e9cc77e779861e9d29b2f6c421f09a06bb4
DianQK <xiaoqing@dianqk.org>
2016-08-16 12:42:52 +0800

    pubilc

--- WebPOC/WebPOC.h
+++ WebPOC/WebPOC.h
@@ -15,5 +15,4 @@ FOUNDATION_EXPORT double WebPOCVersionNumber;
 FOUNDATION_EXPORT const unsigned char WebPOCVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WebPOC/PublicHeader.h>
-//#import "UIImage+WebP.h"
 #import <WebPOC/UIImage+WebP.h>
